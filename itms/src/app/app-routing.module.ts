a2a0188e6a56152aae10c656adf2b8fc200e1ebc
vinay1028 <vinaysingh1028@gmail.com>
2021-08-17 05:28:44 -0700

    updated by vinay

--- itms/src/app/app-routing.module.ts
+++ itms/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './master/product/components/product-list/product-list.component';
+import { ZoneFormComponent } from './master/zone/components/zone-form/zone-form.component';
+import { ZoneListComponent } from './master/zone/components/zone-list/zone-list.component';
 import { ContactsComponent } from './shared/contacts/contacts.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { ForgetPasswordComponent } from './user-authentication/forget-password/forget-password.component';
@@ -10,7 +12,9 @@ const routes: Routes = [
   {path:'login/header', component:HeaderComponent, 
     children:[
       {path:'contacts', component:ContactsComponent},
-      {path:'product', component:ProductListComponent}
+      {path:'product', component:ProductListComponent},
+      {path:'zone-master', component:ZoneListComponent},
+      {path:'zone-form', component:ZoneFormComponent}
     ]
 },
   {path:'login', component:LoginComponent},
